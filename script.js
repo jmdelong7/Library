237cc4a11c8a39cc17e4304aa8bafe73362eaa75
Jacob DeLong <62762261+jmdelong7@users.noreply.github.com>
2024-06-04 19:48:59 -0700

    Constructor Book and addBookToLibrary()
    new Book() takes book information into object.
    addBookToLibrary adds the book from user input to the library array.

---
+++ script.js
@@ -0,0 +1,23 @@
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title,
+  this.author = author,
+  this.pages = pages,
+  this.read = read ? "I've read this book." : "I've yet to read this book."
+}
+
+function addBookToLibrary() {
+  let userTitle = prompt("Enter the title of the book.");
+  let userAuthor = prompt("Enter the book's author");
+  let userPages= prompt("Enter the number of pages in the book.");
+  let userRead = prompt("Have you read the book? (y/n)");
+
+  let newBook = new Book(userTitle, userAuthor, userPages, userRead);
+  myLibrary.push(newBook)
+}
+
+function displayPage() {
+  // loop through myLibrary and display each book on the page.
+}
+
