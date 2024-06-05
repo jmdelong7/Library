7548a041fc02a71ca3e76a3c5d36052ac0be3855
Jacob DeLong <62762261+jmdelong7@users.noreply.github.com>
2024-06-04 21:12:08 -0700

    displayLibrary() shows myLibrary array on page
    Loop through myLibrary and access object properties.
    Puts properties in main table under associated columns.

--- script.js
+++ script.js
@@ -17,7 +17,39 @@ function addBookToLibrary() {
   myLibrary.push(newBook);
 }
 
-function displayPage() {
-  // loop through myLibrary and display each book on the page.
-}
+const tableLibrary = document.querySelector(".table-library");
 
+function displayLibrary() {
+
+  myLibrary.forEach((b) => {
+    const tableRow = document.createElement('tr');
+    
+    const bookIndex = document.createElement('td');
+    bookIndex.classList.add("index");
+    bookIndex.textContent = myLibrary.indexOf(b + 1);
+    
+    const bookTitle = document.createElement('td');
+    bookTitle.classList.add("title");
+    bookTitle.textContent = b.title;
+
+    const bookAuthor = document.createElement('td');
+    bookAuthor.classList.add("author");
+    bookAuthor.textContent = b.author;
+
+    const bookPages = document.createElement('td');
+    bookPages.classList.add("pages");
+    bookPages.textContent = b.pages;
+    
+    const bookRead = document.createElement('td');
+    bookRead.classList.add("read");
+    bookRead.textContent = b.read;
+
+    tableRow.appendChild(bookIndex)
+    tableRow.appendChild(bookTitle);
+    tableRow.appendChild(bookAuthor);
+    tableRow.appendChild(bookPages);
+    tableRow.appendChild(bookRead);
+
+    tableLibrary.appendChild(tableRow);
+  })
+}
