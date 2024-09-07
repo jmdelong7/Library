a0079c19542807daa85bd538b0c9c4deabe5ac9b
Jacob DeLong <62762261+jmdelong7@users.noreply.github.com>
2024-09-06 19:15:31 -0700

    Library displayBook(book)

--- script.js
+++ script.js
@@ -6,14 +6,15 @@ class Book {
     this.hasRead = hasRead
   }
 
-  changeHasRead() {
+  toggleRead() {
     this.hasRead = !this.hasRead
   }
 }
 
 class Library {
-  constructor() {
+  constructor(tableId) {
     this.books = []
+    this.table = document.getElementById(tableId)
   }
 
   addBook(book) {
@@ -21,215 +22,36 @@ class Library {
   }
 
   removeBook(index) {
-    this.books = this.books.filter(book => this.books.indexOf(book) !== index)
+    this.books.splice(index, 1)
   }
-}
 
-class FormHandler {
-  constructor(formId, library) {
-    this.formElement = document.getElementById(formId)
-    this.library = library
-    this.onSubmit()
-  }
-  
-  getFormEntries(formData) {
-    const formEntries = []
-    for (let pair of formData.entries()) {
-      formEntries.push(pair[1])
+  displayBook(book) {
+    const row = this.table.insertRow()
+    const bookData = [book.title, book.author, book.numOfPages, book.hasRead]
+
+    for(let data of bookData) {
+      const cell = row.insertCell()
+      cell.textContent = data
     }
-    return formEntries
+
+    row.insertCell()
+    row.insertCell(0)
   }
 
-  onSubmit() {
-    this.formElement.addEventListener("submit", (event) => {
-      event.preventDefault()
-      let formData = new FormData(this.formElement)
-      this.addBookToLibrary(this.makeBookFromForm(formData))
-      this.formElement.reset()
-    })
+  addActionButtons() {
+
   }
 
-  makeBookFromForm(formData) {
-    let formDataEntries = this.getFormEntries(formData)
-    return new Book(...formDataEntries)
-  }
-
-  addBookToLibrary(book) {
-    this.library.addBook(book)
-  }
-}
-
-class ModalHandler {
-  constructor(addBookButtonElement, dialogElement, cancelButtonElement) {
-    this.addBookButton = document.getElementById(addBookButtonElement)
-    this.dialogElement = document.getElementById(dialogElement)
-    this.cancelButton = document.getElementById(cancelButtonElement)
-    
-    this.addBookListener()
-    this.cancelButtonListener()
-  }
-
-  addBookListener() {
-    this.addBookButton.addEventListener("click", () => {
-      this.dialogElement.showModal()
-    })
-  }
-
-  cancelButtonListener() {
-    this.cancelButton.addEventListener("click", () => {
-      this.dialogElement.close()
+  displayLibrary() {
+    this.books.forEach(book => {
+      const bookRow = this.table.insertRow()
+      
+      for(let [key, value] of Object.entries(book)) {
+        const cell = bookRow.insertCell()
+        cell.className = key
+        cell.textContent = value
+      }
     })
   }
 }
-
-class LibraryDisplay {
-  constructor(library, libraryID) {
-    this.library = library
-    this.libraryElement = document.getElementById(libraryID)
-  }
-
-  createTableColumn() {
-    return document.createElement('td')
-  }
-
-  createBookData(book) {
-    const bookElements = {}
-
-    const removeButton = document.createElement('button')
-    const removeCol = this.createTableColumn()
-    removeButton.textContent = "Remove"
-    removeButton.className = "remove-button"
-    removeCol.appendChild(removeButton)
-    bookElements["remove"] = removeCol
-
-    const bookIdx = this.library.books.indexOf(book)
-    removeButton.addEventListener('click', this.library.removeBook(bookIdx))
-    
-    for (let [key, value] of Object.entries(book)) {
-      const tData = this.createTableColumn()
-      tData.textContent = value
-      bookElements[key] = tData
-    }
-    
-    const changeButton = document.createElement('button')
-    const changeCol = this.createTableColumn()
-    changeButton.textContent = "Change"
-    changeButton.className = "change-button"
-    changeCol.appendChild(changeButton)
-    bookElements["change"] = changeCol
-    
-    return bookElements
-  }
-  
-  createTableRow(bookElements) {
-    const tableRow = document.createElement('tr')
-    Object.keys(bookElements).forEach( (key) => {
-      tableRow.appendChild(bookElements[key])
-    })
-    this.libraryElement.appendChild(tableRow)
-  }
-
-}
-
-const modal = new ModalHandler("add-book", "form-dialog", "cancel-form", "submit-form")
-const library = new Library()
-const formData = new FormHandler("book-form", library)
-const libraryDisplay = new LibraryDisplay(library, "table-library")
-
-/* 
-
-(function addBookCancelButtonListeners() {
-  const addBookButton = document.getElementById("add-book")
-  const dialogElement = document.getElementById("form-dialog")
-  addBookButton.addEventListener("click", () => {
-    dialogElement.showModal()
-  })
-
-  const cancelButton = document.getElementById("cancel-form")
-  cancelButton.addEventListener("click", () => {
-    dialogElement.close()
-  })
-})()
-
-*/
-
-
-
-
-
-function displayLibrary() {
-
-  const books = document.querySelectorAll("tr.book");
-  for (const book of books) {
-    book.parentNode.removeChild(book);
-  };  
-
-  myLibrary.forEach((b) => {
-    const tableRow = document.createElement('tr');
-    tableRow.classList.add("book");
-    const libraryIndex = myLibrary.indexOf(b) + 1;
-    const indexClassName = "book-" + libraryIndex;
-
-    const bookRemove = document.createElement('td');
-    bookRemove.classList.add("remove", indexClassName);
-    const removeButton = document.createElement('button');
-    removeButton.textContent = "Remove";
-    bookRemove.appendChild(removeButton);
-        
-    const bookTitle = document.createElement('td');
-    bookTitle.classList.add("title", indexClassName);
-    bookTitle.textContent = b.title;
-
-    const bookAuthor = document.createElement('td');
-    bookAuthor.classList.add("author", indexClassName);
-    bookAuthor.textContent = b.author;
-
-    const bookPages = document.createElement('td');
-    bookPages.classList.add("pages", indexClassName);
-    bookPages.textContent = b.pages;
-    
-    const bookRead = document.createElement('td');
-    bookRead.classList.add("read", indexClassName);
-    bookRead.textContent = b.read;
-    
-    const bookChange = document.createElement('td');
-    const changeButton = document.createElement('button');
-    bookChange.classList.add("change", indexClassName);
-    changeButton.textContent = "Change";
-    bookChange.appendChild(changeButton)
-
-    tableRow.appendChild(bookRemove);
-    tableRow.appendChild(bookTitle);
-    tableRow.appendChild(bookAuthor);
-    tableRow.appendChild(bookPages);
-    tableRow.appendChild(bookRead);
-    tableRow.appendChild(bookChange);
-
-    tableLibrary.appendChild(tableRow);
-
-    removeBook(removeButton, tableRow);
-    changeButton.addEventListener("click", () => {
-      changeReadStatus(b);
-      bookRead.textContent = b.read;
-    });
-  })
-}
-
-function removeBook(bookRemoveBtn, tableRow) {
-  bookRemoveBtn.addEventListener("click", () => {
-    tableLibrary.removeChild(tableRow);
-  });
-}
-
-function changeReadStatus(book) {
-  let bookReadText = book.read; // I've read or I've not read
-  book.changeReadStatus(false); // I've not read
-  if (bookReadText === book.read) { // If both false
-    book.changeReadStatus(true); // Change true
-  } else {book.changeReadStatus(false)} // Else change false
-}
-
-
-
-
 
